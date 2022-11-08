d3ee05125f4d19bc8f65112fe05ddc561bfa706d
trevorlink <lvpidadiao@hotmail.com>
2022-11-08 14:47:34 +0800

    fix conflict, change this crate to library, but it should be a bin when publish

--- src/department/tui/term.rs
+++ src/department/tui/term.rs
@@ -1,28 +1,44 @@
+
 use std::io;
+use std::io::Stdout;
+use std::sync::mpsc::Receiver;
+use std::time::Duration;
 use tui::{backend::CrosstermBackend, Frame, Terminal, TerminalOptions};
 use tui::backend::Backend;
+use tokio;
+use tokio::runtime::Runtime;
 
-pub struct TermShader<W> {
-    terminal: Terminal<CrosstermBackend<W>>
+pub struct TermRenderer
+{
+    keyboard_rx: Receiver<String>,
+    rt: Runtime
 }
 
 
-impl TermShader<W> {
-    pub fn new() ->Self {
-        let stdout = std::io::stdout();
-        let backend=  CrosstermBackend::new(stdout);
-        TermShader{
-            terminal: Terminal::new(backend).unwrap()
+impl TermRenderer where {
+    pub fn new_with(rx: Receiver<String>) ->Self {
+        let runtime = tokio::runtime::Builder::new_current_thread().build().unwrap();
+        TermRenderer {
+            keyboard_rx: rx,
+            rt: runtime
         }
     }
 
+    pub fn run(mut self) {
+        self.rt.block_on(async {
+            &self.update();
+        });
+    }
 
-    pub fn update<B:Backend>(mut self) {
-        self.terminal.draw(|f| ui(f, &self))?;
-
+    async fn update(&self) {
+        loop {
+            if let Ok(s) = self.keyboard_rx.recv_timeout(Duration::from_secs(1)) {
+                
+            }
+        }
     }
 }
 
-fn ui<B:Backend>(f: &mut Frame<B>, render: &TermShader<B>) {
+fn ui<B:Backend>(f: &mut Frame<B>, render: &TermRenderer) {
 
 }
