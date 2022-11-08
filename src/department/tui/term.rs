5969441874273859c06717fa77a3ff4a1f361a71
trevorlink <lvpidadiao@hotmail.com>
2022-11-08 11:47:37 +0800

    add xmind doc

--- src/department/tui/term.rs
+++ src/department/tui/term.rs
@@ -1,17 +1,28 @@
 use std::io;
-use tui::{backend::CrosstermBackend, Terminal};
+use tui::{backend::CrosstermBackend, Frame, Terminal, TerminalOptions};
+use tui::backend::Backend;
 
-pub struct TermRenderer<W> {
+pub struct TermShader<W> {
     terminal: Terminal<CrosstermBackend<W>>
 }
 
 
-impl TermRenderer<W> {
+impl TermShader<W> {
     pub fn new() ->Self {
         let stdout = std::io::stdout();
         let backend=  CrosstermBackend::new(stdout);
-        TermRenderer{
+        TermShader{
             terminal: Terminal::new(backend).unwrap()
         }
     }
+
+
+    pub fn update<B:Backend>(mut self) {
+        self.terminal.draw(|f| ui(f, &self))?;
+
+    }
+}
+
+fn ui<B:Backend>(f: &mut Frame<B>, render: &TermShader<B>) {
+
 }
